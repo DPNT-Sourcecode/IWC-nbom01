94dabcf19628ec44bb42a4b3c6b23a78a11ce08b
sbx_user1051 <sbx_user1051@169.254.32.1>
2026-02-25 19:32:03 +0000

    Wed Feb 25 19:32:03 UTC 2026

--- test/solution_tests/IWC/test_queue_solution.py
+++ test/solution_tests/IWC/test_queue_solution.py
@@ -105,14 +105,11 @@ def test_deprioritizing_bank_statements() -> None:
     run_queue([
         call_enqueue("bank_statements", 1, iso_ts(delta_minutes=0)).expect(1),
         call_enqueue("id_verification", 1, iso_ts(delta_minutes=1)).expect(2),
-        call_enqueue("companies_house",2, iso_ts(delta_minutes=2)).expect(2),
+        call_enqueue("companies_house", 2, iso_ts(delta_minutes=2)).expect(3),
          call_size().expect(3),
         call_dequeue().expect("id_verification", 1),
-        call_dequeue().expect("id_verification", 2),
+        call_dequeue().expect("companies_house", 2),
         call_dequeue().expect("bank_statements", 1),
         call_size().expect(0),
     ])
-
-
-
 
